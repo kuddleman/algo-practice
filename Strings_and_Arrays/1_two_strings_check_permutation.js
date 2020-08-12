1f71ea5b16d47b735ace3b127837cb90b39e8011
Kuddleman <edl1155@gmail.com>
2020-08-12 08:30:54 -0700

    solve two strings check for permutation

---
+++ Strings_and_Arrays/1_two_strings_check_permutation.js
@@ -0,0 +1,34 @@
+/*
+https://www.geeksforgeeks.org/check-if-two-strings-are-permutation-of-each-other/#:~:text=Write%20a%20function%20to%20check,are%20Permutation%20of%20each%20other.
+``````````````````
+Write a function to check whether two given strings 
+are Permutation of each other or not. A Permutation 
+of a string is another string that contains same characters, 
+only the order of characters can be different. 
+
+For example, “abcd” and “dabc” are Permutation of each other.
+*/
+
+
+const isPermutation = ( str1, str2 ) => {
+
+  //first check lengths of each string. If 
+  // lengths are NOT equal, then they cannot be permutations
+  // return FALSE
+
+
+
+  // convert each string to an array
+
+
+  //sort each array alphabetically
+
+
+  //loop through one array and compare each letter to
+  // the corresponding letter in the 2nd array. If the
+  // elements being compared are not strictly equal, then
+  // return FALSE
+
+
+  // After the loop is completed, return TRUE 
+}
